60339a038ba5b3b1d0b4ea21d09aa1ae8a77ea03
appnalitica <francisco.garcia.ti@gmail.com>
2015-01-05 23:18:09 +0100

    contadores
    Número de cartas seleccionadas se muestra en el tablero

--- app/lk/canvas/canvas.controller.js
+++ app/lk/canvas/canvas.controller.js
@@ -4,7 +4,29 @@
 		.module('lkCanvas')
 		.controller('CanvasController' , canvasController);
 
-	canvasController.$inject = [];
-	function canvasController( ) { 
+	canvasController.$inject = ['Mazo'];
+
+	function canvasController( Mazo ) { 
+		var canvas = this;
+		canvas.numOfSelected = numOfSelected;
+
+		function numOfSelected(nombreMazo) {
+			var cartas, cuantas=0;
+			if(nombreMazo === 'disparadores') {
+				cartas = Mazo.disparadores.cartas;
+			} else if(nombreMazo === 'motivadores') {
+				cartas = Mazo.motivadores.cartas;
+			} else if(nombreMazo === 'mecanicas') {
+				cartas = Mazo.mecanicas.cartas;
+			} else if(nombreMazo === 'componentes') {
+				cartas = Mazo.componentes.cartas;
+			} else{
+				return null;
+			}
+			for(var i in cartas) {
+				if(cartas[i].seleccionado) cuantas++;
+			}
+			return (!cuantas)? 'No hay ninguna carta seleccionada todavía': 'Has seleccionado '+cuantas;
+		}
 	}
 })()
