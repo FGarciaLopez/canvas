2c351c7648a2a98aaf14628150a0d7dfca2660b7
Francisco García <francisco.garcia.ti@gmail.com>
2015-02-19 12:50:25 +0100

    mejora menu

--- app/lk/menuLateral/menuLateral.directive.js
+++ app/lk/menuLateral/menuLateral.directive.js
@@ -25,19 +25,22 @@
 		submn.estaActivoModoSeleccion = StatusService.estaActivadoModoSeleccion;
 		submn.estaCompletado = estaCompletado;
 		submn.estaEnModoSeleccion = StatusService.estaEnModoSeleccion;
-		submn.estaEnTablero = estaEnTablero;
-		submn.getAnterior = StatusService.pasoAnterior;
-		submn.getSiguiente = StatusService.pasoSiguiente;
-		submn.goHome = goHome;
-		submn.irAnterior = irAnterior;
-		submn.irSiguiente = StatusService.cambiarPaso;
+		//submn.estaEnTablero = estaEnTablero;
+		//submn.getAnterior = StatusService.pasoAnterior;
+		//submn.getSiguiente = StatusService.pasoSiguiente;
+		//submn.goHome = goHome;
+		//submn.irAnterior = irAnterior;
+		submn.irSiguiente = irSiguiente;
 		submn.estaActivo = estaActivo;
 		submn.pasos = StatusService.pasos;
-		submn.tipo = $scope.tipo;
 
-		function estaEnTablero() { return (StatusService.pasoActual()==='Tablero') }
-		function goHome() { StatusService.cambiarPaso('Tablero') }
-		function irAnterior() { StatusService.cambiarPaso( StatusService.pasoAnterior()) }
+		//function estaEnTablero() { return (StatusService.pasoActual()==='Tablero') }
+		//function goHome() { StatusService.cambiarPaso('Tablero'); submn.close(); }
+		//function irAnterior() { StatusService.cambiarPaso( StatusService.pasoAnterior()) }
+		function irSiguiente( nombre) {
+			StatusService.cambiarPaso( nombre);
+			submn.close();
+		}
 		function estaActivo( nombrePaso) { return StatusService.pasoActual() === nombrePaso}
 		function estaCompletado() {
 			if (! MazoService.algunaSeleccionada( MazoService.get('componentes').cartas)) {
