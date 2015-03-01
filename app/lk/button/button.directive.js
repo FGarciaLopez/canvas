ceabbad5c6ee50e296abf2b4d299ece7c2b6aace
Francisco García <francisco.garcia.ti@gmail.com>
2015-03-01 23:19:06 +0100

    1ªver dialogo modal

--- app/lk/button/button.directive.js
+++ app/lk/button/button.directive.js
@@ -9,15 +9,17 @@
 	    
 	    function lkButtonLink(scope, element, attrs) {
 			var ink, d, x, y;
-			element.parent().css( {overflow: 'hidden', position: 'relative'});
+			element.css( {overflow: 'hidden'});
 			ink = element.prepend("<span class='lk-ink'></span>").find('.lk-ink');
 			element.click(function(e){
 				if(this.className.indexOf('u-disabled')>0) {
 					return;
 				}
-				//incase of quick double clicks stop the previous animation
+
+				console.log("evento click", e, element);
+				// Si se hacen dos clicks seguidos para la animación anterior
 				ink.removeClass("animate");
-				//set size of .ink
+				// Pone el tamaño de .lk-ink
 				if(!ink.height() && !ink.width())
 				{
 					//use eelement's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
@@ -25,11 +27,12 @@
 					ink.css({height: d, width: d});
 				}
 				
-				//get click coordinates
+				// Toma las coordenadas del click
 				//logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
 				x = e.pageX - element.offset().left - ink.width()/2;
 				y = e.pageY - element.offset().top - ink.height()/2;
 				//set the position and add class .animate
+
 				ink.css({top: y+'px', left: x+'px'}).addClass("animate");
 			})
 	    }
